9db399b9ba6acf45fef9e8e6c689b2ffd35f222f
Kenenisa Jaleto <kenenisajaleto7@gmail.com>
2026-03-12 15:52:03 +0300

    rows

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -2,12 +2,14 @@ import React from "react";
 import Header from "../../Components/Header/header";
 import Footer from "../../Components/Footer/footer";
 import Banner from "../../Components/Banner/banner";
+import RowList from "../../Components/Rows/RowList/rowlist";
 
 const Home = () => {
   return (
     <>
       <Header />
       <Banner />
+      <RowList />
       <Footer />
     </>
   );
