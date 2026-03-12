03708bb4eb5bf25476d332f4780f2df56a7edddc
Kenenisa Jaleto <kenenisajaleto7@gmail.com>
2026-03-12 14:31:58 +0300

    banner

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../../Components/Header/header";
 import Footer from "../../Components/Footer/footer";
+import Banner from "../../Components/Banner/banner";
 
 const Home = () => {
   return (
     <>
       <Header />
+      <Banner />
       <Footer />
     </>
   );
